8dc4c049d8b5177555100c7b142d54a0f2848385
Matthias <10801000+matthias-brun@users.noreply.github.com>
2022-09-12 10:23:42 +0200

    make things work on newest verus

--- page-table/main.rs
+++ page-table/main.rs
@@ -2,14 +2,8 @@
 extern crate alloc;
 
 pub mod lib;
-pub mod lib_axiom;
-pub mod memory_types;
 pub mod pervasive;
 pub mod pt_impl;
-pub mod system;
-pub mod pt;
-pub mod high_level_spec;
 pub mod aux_defs;
-pub mod mem;
 
 fn main() {}
--- page-table/pt_impl/mod.rs
+++ page-table/pt_impl/mod.rs
@@ -1,3 +1,2 @@
 pub mod l0;
 pub mod l1;
-pub mod l2_impl;
